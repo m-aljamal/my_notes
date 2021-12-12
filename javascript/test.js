e8a4672bf9196aa71e17ce32c036ba666d92b685
m-aljamal <mohammadjamol@gmail.com>
2021-12-12 22:32:38 +0300

    create project database

--- javascript/test.js
+++ javascript/test.js
@@ -1,61 +1,36 @@
-// const findAnagrams = (str1, str2) => {
-//   if (str1.length !== str2.length) return false;
-//   const str1Obj = {};
-//   const str2Obj = {};
-//   for (let i = 0; i < str1.length; i++) {
-//     str1Obj[str1[i]] = ++str1Obj[str1[i]] || 1;
-//     str2Obj[str2[i]] = ++str2Obj[str2[i]] || 1;
-//   }
-
-//   for (let key in str1Obj) {
-//     if (str1Obj[key] !== str2Obj[key]) return false;
-//   }
-//   return true;
-// };
-// console.log(findAnagrams("abcd", "dcba"));
-// console.log(findAnagrams("", ""));
-// console.log(findAnagrams("aaz", "zza"));
-// console.log(findAnagrams("anagram", "nagaram"));
-// console.log(findAnagrams("rat", "car"));
-
-function sunZero(arr) {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {
-    let sum = arr[left] + arr[right];
-    if (sum === 0) {
-      return [arr[left], arr[right]];
-    } else if (sum > 0) {
-      right--;
-    } else {
-      left++;
-    }
-  }
-}
-
-// console.log(sunZero([-4, -3, -2, 1, 2, 4]));
-
-// function findUniqu(arr = []) {
-//   let i = 0;
-//   for (let j = 1; j < arr.length; j++) {
-//     if (arr[i] !== arr[j]) {
-//       i++;
-//       arr[i] = arr[j];
-//     }
-//   }
-//   return i + 1
-// }
-
-// console.log(findUniqu([1, 1, 1, 2]));
-
-function narcissistic(value) {
-  const arr = value.toString().split("");
-  let total = arr.reduce((prev, curr) => +prev + Math.pow(+curr, +arr.length),0);
+function isPangram(string) {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
   
-  return total === value;
+  const l = alphabet.every((letter) => string.toUpperCase().includes(letter));
+  console.log(l);
 }
-// 27  343  1
-console.log(narcissistic(371));
 
-
- 
+console.log(isPangram("The quick brown fox jumps over the lazy dog."));
+// alphabet.includes(letter)
